005a572fdfa390d27a8b549b80d56843713da15b
diamondburned <diamond@arikawa-hi.me>
2023-04-22 01:09:19 -0700

    Add Nix shell files

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+	unstable = import (pkgs.fetchFromGitHub {
+		owner = "NixOS";
+		repo = "nixpkgs";
+		rev = "f00994e78cd39e6fc966f0c4103f908e63284780"; # nixos-unstable
+		sha256 = "0kpnpja0pv4bk12iqia6avll31i85327p5drs2ycni14qa166y54";
+	}) {};
+in
+
+pkgs.mkShell {
+	buildInputs = with unstable; [
+		python311
+	];
+
+	shellHook = ''
+		python -m venv .venv
+		source .venv/bin/activate
+		pip install -r requirements.txt
+	'';
+}
